ee42f2b42ce414c851de384af7c04ce516eedf9f
kawatson <katelynwatson@u.boisestate.edu>
2015-10-09 18:13:23 -0600

    Add some comments
    Add comments to Module 06 example.

--- Module06/Example06.py
+++ Module06/Example06.py
@@ -1,4 +1,20 @@
+""" 
+To run this code in a terminal window or the
+Anaconda Command Prompt type:
+
+python Example06.py "argument"
+
+or in the IPython console type:
+
+run Example06.py "argument"
+
+Note: You will need to be in the same directory
+as the this Python script when you enter these
+commands.
+
+"""
+
 import sys
 
-x = sys.argv[1]
+x = sys.argv[1]  # save command line input as x
 print x
